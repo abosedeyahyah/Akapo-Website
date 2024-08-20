2dbb432de1fffd92f0923bb26339d31e162d80ff
abosedeyahyah <abosedeyahyah@gmail.com>
2024-08-20 20:43:33 +0100

    works

--- app/Components/TeamPage.jsx
+++ app/Components/TeamPage.jsx
@@ -65,7 +65,7 @@ const TeamPage = () => {
       </section>
 
       <section className="bg-[#F2F7F4] py-12">
-      <div className="">
+        <div className="">
           <div className="text-center">
             <h1 className="text-lg md:text-4xl max-w-xl mb-5 Montserrat text-[#000000] text-center mx-auto font-medium">
               Meet Our Dedicated Team
@@ -81,20 +81,35 @@ const TeamPage = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
             <div className="text-center">
               <h4 className="text-4xl font-bold text-[#00A874]">10k+</h4>
-              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">Years of Service</p>
-              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">Providing reliable financial solutions</p>
+              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">
+                Years of Service
+              </p>
+              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">
+                Providing reliable financial solutions
+              </p>
             </div>
             <div className="text-center">
               <h4 className="text-4xl font-bold text-[#00A874]">1000+</h4>
-              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">Happy Members</p>
-              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">Helping members achieve their financial goals</p>
+              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">
+                Happy Members
+              </p>
+              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">
+                Helping members achieve their financial goals
+              </p>
             </div>
             <div className="text-center">
               <h4 className="text-4xl font-bold text-[#00A874]">100M+</h4>
-              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">Save Funds</p>
-              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">Helping members achieve their financial goals</p>
+              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">
+                Save Funds
+              </p>
+              <p className="mt-2 text-base md:text-lg lg:text-xl max-w-xl">
+                Helping members achieve their financial goals
+              </p>
             </div>
           </div>
+        </div>
+        <div className="text-center mt-5">
+          <h1>Members Reviews</h1>
         </div>
       </section>
     </div>
